8663a82a3a8d982ae35e3068060e3c1594df5dc4
TakrimJengi <133348354+TakrimJengi@users.noreply.github.com>
2026-06-09 09:50:49 +0600

    Add Product Model, Category Model

---
+++ backend/models/Product.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/db');
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  purchase_price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  selling_price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  stock_quantity: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  },
+  minimum_stock: {
+    type: DataTypes.INTEGER,
+    defaultValue: 5
+  }
+}, {
+  timestamps: true
+});
+
+module.exports = Product;
